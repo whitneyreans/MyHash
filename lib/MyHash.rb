ab053e1249ed6421e2a1aa93fe032a76b3825dd7
Whitney Reans & David Zulaica <zulaica@gmail.com>
2015-01-12 11:59:01 -0800

    Define fetch and store methods

--- lib/MyHash.rb
+++ lib/MyHash.rb
@@ -1,10 +1,19 @@
 class MyHash
-  define_method(:initialize) do |key, value|
-    @key = key
-    @value = value
+  define_method(:initialize) do
+    @hash = []
   end
 
-  define_method(:store) do
-    @value
+  define_method(:store) do |key, value|
+    @hash.push([key, value])
+  end
+
+  define_method(:fetch) do |key|
+    value = nil
+    @hash.each() do |pair|
+      if pair.at(0) == key
+        value = pair.at(1)
+      end
+    end
+    value
   end
 end
