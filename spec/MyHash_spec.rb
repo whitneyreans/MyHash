45c1092fd23d4099cb9ff7ba153c92090ed8da7e
Whitney Reans & David Zulaica <zulaica@gmail.com>
2015-01-12 11:13:50 -0800

    Revise spec to test store and fetch methods.

--- spec/MyHash_spec.rb
+++ spec/MyHash_spec.rb
@@ -2,17 +2,11 @@ require('rspec')
 require('MyHash')
 
 describe("MyHash") do
-  describe("#store") do
-    it("store a key/value pair and returns its value") do
-      test_hash = MyHash.new("kitten","cute")
-      expect(test_hash.store()).to(eq("cute"))
+  describe("#fetch") do
+    it("retrieves a stored value by its key") do
+      test_hash = MyHash.new()
+      test_hash.store("kitten", "cute")
+      expect((test_hash).fetch("kitten")).to(eq("cute"))
     end
   end
-#  describe("#fetch") do
-#    it("retrieves a stored value by its key") do
-#      test_hash = MyHash.new()
-#      test_hash.store("kitten", "cute")
-#      expect((test_hash).fetch("kitten")).to(eq("cute"))
-#    end
-#  end
 end
